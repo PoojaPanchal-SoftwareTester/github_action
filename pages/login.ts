82619ce0929945d8b6b63b70979ce8e2be4da7cc
poojapanchal <pj2491@gmail.com>
2026-06-26 12:20:50 +0530

    changes

--- pages/login.ts
+++ pages/login.ts
@@ -2,59 +2,61 @@ import { Locator, Page } from "@playwright/test";
 import playwrightConfig from "../playwright.config";
 import { HomePage } from "./homePage";
 
-export class LoginPage{
+export class LoginPage {
 
     readonly page: Page;
-    readonly emailInput : Locator;
+    readonly emailInput: Locator;
     readonly password: Locator;
     readonly loginBtn: Locator;
-    readonly homeLoginBtn:Locator;
-    readonly homeTitle:Locator;
-     
-    constructor(page:Page)
-    {
+    readonly homeLoginBtn: Locator;
+    readonly homeTitle: Locator;
+
+    constructor(page: Page) {
         this.page = page;
-        this.homeLoginBtn= page.getByRole('link', { name: 'Login', exact: true });
+        this.homeLoginBtn = page.getByRole('link', { name: 'Login', exact: true });
         this.emailInput = page.getByRole('textbox', { name: 'Enter your active Email ID /' });
         this.password = page.getByRole('textbox', { name: 'Enter your password' });
         this.loginBtn = page.getByRole('button', { name: 'Login', exact: true });
         this.homeTitle = page.locator('.info__heading');
 
-    } 
-
-    async open()
-    {
-  const baseUrl = process.env.BASE_URL || '/';
-        await this.page.goto(baseUrl);
-       // await this.page.goto('/')
-         //await this.page.pause();
-        // console.error('Login failed - Login button not found. Current URL:', this.page.url());
     }
-    
-    async loginForm(email:string,pass:string) 
-    {
-        try{
-     await this.homeLoginBtn.waitFor({ state: 'visible' });
-    await this.homeLoginBtn.click();
 
-      await this.emailInput.waitFor({ state: 'visible' });
-      await this.emailInput.fill(email);
+    async open() {
 
-      await this.password.fill(pass);
-     await this.loginBtn.click();
-        await this.emailInput.fill(email);
-        await this.password.click();
-        await this.password.fill(pass);
-        await this.loginBtn.click();
-        
-        return HomePage;
+        if(process.env.BASE_URL)
+        {
+           await this.page.goto(process.env.BASE_URL); 
         }
-        catch(error){
-console.error('Login failed - Login button not found. Current URL:', this.page.url());
-        throw error;
-        }
-         
+         else{
+          console.error('BaseUrl not found:', this.page.url());
+         }
         
+    }
+
+    async loginForm(email: string, pass: string) {
+        try {
+            await this.homeLoginBtn.waitFor({ state: 'visible' });
+            await this.homeLoginBtn.click();
+
+            await this.emailInput.waitFor({ state: 'visible' });
+            await this.emailInput.click();
+            await this.emailInput.fill(email);
+
+            await this.password.waitFor({ state: 'visible' });
+            await this.password.click();
+            await this.password.fill(pass);
+
+            await this.loginBtn.waitFor({ state: 'visible' });
+            await this.loginBtn.click();
+
+            return HomePage;
+        }
+        catch (error) {
+            console.error('Login failed - Login button not found. Current URL:', this.page.url());
+            throw error;
+        }
+
+
     }
 
 }
